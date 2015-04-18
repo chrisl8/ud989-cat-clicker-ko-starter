983120ea6265d1fb1df37c839d0af474722e4f7e
Chris L8 <chrisl8@users.noreply.github.com>
2015-04-18 17:37:19 -0500

    Add nickname list.

--- js/app.js
+++ js/app.js
@@ -13,6 +13,14 @@ var ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('http://www.flickr.com/photos/big');
+    this.nickNames = ko.observableArray([
+        { name: 'Tabtab' },
+        { name: 'T-Bone' },
+        { name: 'Mr. T' },
+        { name: 'Tabitha Tab Tabby Catty Cat' }
+    ]);
+            /* Use "text: $data" if it is an array,
+            and not an object. */
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
