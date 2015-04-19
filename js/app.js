9f6c016269b3bdc617020cc4624869c2a0285014
Chris L8 <chrisl8@users.noreply.github.com>
2015-04-19 17:06:53 -0500

    Add multiple cats.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+var initialCats = [{
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nickNames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+}, {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+    nickNames: ['Tigger']
+}, {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+    nickNames: ['Casper']
+}, {
+    clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+    nickNames: ['Shooby']
+}, {
+    clickCount: 0,
+    name: 'Sleepy',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+    nickNames: ['Zzzzz']
+}];
+
 var Cat = function(data) {
     this.clickCount = ko.observable(data.clickCount);
     this.level = ko.computed(function() {
@@ -17,17 +49,22 @@ var Cat = function(data) {
 };
 
 var ViewModel = function() {
+    var self = this;
 
-    this.currentCat = ko.observable(new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: 'http://www.flickr.com/photos/big',
-        nickNames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
-    }));
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
+    };
+
+    this.setCurrentCat = function(data) {
+        self.currentCat(data);
     };
 };
 
