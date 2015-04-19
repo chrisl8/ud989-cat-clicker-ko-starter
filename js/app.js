8cad006de291c058c7292fe82c0a750ad5fb27a0
Chris L8 <chrisl8@users.noreply.github.com>
2015-04-19 10:53:41 -0500

    Refactor Model for use with multiple cats.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
     this.level = ko.computed(function() {
         if (this.clickCount() > 10) {
             return 'Infant';
@@ -7,23 +7,24 @@ var Cat = function() {
             return 'NewBorn';
         }
     }, this); // Important! ;)
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('http://www.flickr.com/photos/big');
-    this.nickNames = ko.observableArray([
-        { name: 'Tabtab' },
-        { name: 'T-Bone' },
-        { name: 'Mr. T' },
-        { name: 'Tabitha Tab Tabby Catty Cat' }
-    ]);
-            /* Use "text: $data" if it is an array,
-            and not an object. */
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nickNames = ko.observable(data.nickNames);
+    /* Use "text: $data" if it is an array,
+    and not an object. */
 
 };
 
 var ViewModel = function() {
 
-    this.currentCat = ko.observable( new Cat() );
+    this.currentCat = ko.observable(new Cat({
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'http://www.flickr.com/photos/big',
+        nickNames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+    }));
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
